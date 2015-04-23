53c151e50f5face8dc8017c05cae39e5ec187ee0
Anna <aisern91@gmail.com>
2015-04-23 16:56:34 +0200

    pizzes - css - contacte

---
+++ html/contacte.php
@@ -0,0 +1,6 @@
+
+
+<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d2794.158585759926!2d2.632733580596855!3d41.95589165237327!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e1!3m2!1ses!2ses!4v1429800388998" width="600" height="450" frameborder="0" style="border:0"></iframe>
+
+
+
--- html/header.php
+++ html/header.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ca">
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,6 +7,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
     <link rel="icon" href="../img/favicon.ico">
+    <link rel="stylesheet" href="../css/estils.css" type="text/css" media="screen">
 
     <title>Pizzeria Happy</title>
 
