e678a709fde8cbe843736f4f4c3e6143920f8148
Anna <aisern91@gmail.com>
2015-04-23 16:29:26 +0200

    prova3

--- html/pizzes.php
+++ html/pizzes.php
@@ -1,8 +1,11 @@
 <?php
 $sx = simplexml_load_file('../data/pizzes.xml'); // Carrega el fitxer XML com un objecte 
-
-foreach($sx->pizza as $item) // Carrega tots els nodes de l'etiqueta "pizza" 
-{
-	echo "<h1>" . $item->id . " => " . $item->nom . "</h1><br>";
-}
+echo "hola1";
+echo "<table>";
+    echo "<tr><th>ID</th><th>NOM</th><th>FOTO</th><th>COMPOSICIÓ</th> </tr>";
+    foreach($sx->pizza as $item) // Carrega tots els nodes de l'etiqueta "pizza" 
+    {
+        echo "<tr><td>" . $item->id . "</td> <td>" . $item->nom . "</td><td><img alt='".$item->nom."' src='../img/pizzes/" . $item->foto . "'></td><td>" . $item->composicio . "</td></tr>";
+    }
+echo "</table>";
 ?>
