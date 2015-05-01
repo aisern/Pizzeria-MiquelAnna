99e85ffc0f9ec416c0616d7d4614c04dae368d11
Anna <aisern91@gmail.com>
2015-05-01 11:19:38 +0200

    contacte

--- html/contacte.php
+++ html/contacte.php
@@ -1,7 +1,20 @@
 <?php include("header.php") ?>
-
+<div>
+    <h1>Local: </h1>
+    <p>Plaça Mare de Déu del Remei, número 4 </p>
+    <p>17160 Anglès</p>
+    <p></p>
+</div>
 <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d2794.158585759926!2d2.632733580596855!3d41.95589165237327!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e1!3m2!1ses!2ses!4v1429800388998" width="600" height="450" frameborder="0" style="border:0"></iframe>
-
-
+<br><br>
+<div>
+    <h1>CONTACTE:</h1>
+    <form>
+        <label>Nom i Cognoms: </label> <input type="text"  name="nom" placeholder="NOM"> <br><br>
+        <label>Correu electrònic: </label ><input type="email" name="mail" placeholder="E-MAIL"> <br><br>
+        <label>Missatge: </label> <textarea name="text" placeholder="MISSATGE"> </textarea><br><br>
+        <input type="button" value="ENVIAR">
+    </form>
+</div>
 
 <?php include("footer.php") ?>
--- html/pizzes.php
+++ html/pizzes.php
@@ -4,10 +4,10 @@
 $sx = simplexml_load_file('../data/pizzes.xml'); // Carrega el fitxer XML com un objecte 
 
 echo "<table>";
-    echo "<tr><th>ID</th><th>NOM</th><th>FOTO</th><th>COMPOSICIÓ</th> </tr>";
+    echo "<tr><th>NOM</th><th>FOTO</th><th>INGREDIENTS</th> </tr>";
     foreach($sx->pizza as $item) // Carrega tots els nodes de l'etiqueta "pizza" 
     {
-        echo "<tr><td>" . $item->id . "</td> <td>" . $item->nom . "</td><td><img alt='".$item->nom."' src='../img/pizzes/" . $item->foto . "'></td><td>" . $item->composicio . "</td></tr>";
+        echo "<tr><td>" . $item->nom . "</td><td><img alt='".$item->nom."' src='../img/pizzes/" . $item->foto . "'></td><td>" . $item->composicio . "</td></tr>";
     }
 echo "</table>";
 ?>
